8d77690c527355f80ce7de68fb1a0ad8c395e606
Nguyen Thanh Tung <ttttung204210@gmail.com>
2018-08-08 20:43:05 +0700

    Initialize

--- Caro/Caro/ChessBoardManager.cs
+++ Caro/Caro/ChessBoardManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,60 @@ namespace Caro
         
         private List<List<Button>> matrix;
         public List<List<Button>> Matrix { get => matrix; set => matrix = value; }
+
+        private event EventHandler<ButtonClickEvent> playerMarked;
+        public event EventHandler<ButtonClickEvent> PlayerMarked
+        {
+            add
+            {
+                playerMarked += value;
+            }
+            remove
+            {
+                playerMarked -= value;
+            }
+        }
+
+        private event EventHandler endedGame;
+        public event EventHandler EndedGame
+        {
+            add
+            {
+                endedGame += value;
+            }
+            remove
+            {
+                endedGame -= value;
+            }
+        }
+
+        private Stack<PlayInfo> playTimeLine;
+        public Stack<PlayInfo> PlayTimeLine { get => playTimeLine; set => playTimeLine = value; }
         #endregion
+
+        #region Initialize
+        public ChessBoardManager(Panel chessBoard,TextBox playerName, PictureBox mark)
+        {
+            this.ChessBoard = chessBoard;
+            this.PlayerName = playerName;
+            this.PlayerMark = mark;
+
+            this.Player = new List<Player>()
+            {
+
+            };
+        }
+        #endregion
+    }
+
+    public class ButtonClickEvent : EventArgs
+    {
+        private Point clickedPoint;
+        public Point ClickedPoint { get => clickedPoint; set => clickedPoint = value; }
+
+        public ButtonClickEvent(Point point)
+        {
+            this.ClickedPoint = point;
+        }
     }
 }
